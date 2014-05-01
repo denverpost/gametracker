470f2598cd0f92ab0976c4a110f1dabf8c8ca463
schneidan <dan@scrollwright.com>
2014-04-30 21:21:09 -0600

    Fixed errant bullet with no time displayed.

--- avs/index.php
+++ avs/index.php
@@ -557,7 +557,7 @@ $config = get_config($fileteam);
                 $('#quarter').html(quarterOut);
                 if (data["team-sport-content"]["league-content"]["competition"]["result-scope"]) {
                     if (data["team-sport-content"]["league-content"]["competition"]["result-scope"]["competition-status"] == 'in-progress') {
-                        if (timeRemaining) { $('#bullet').html('&bull;'); }
+                        if (timeRemaining) { $('#bullet').html('&bull;'); } else { $('#bullet').html(''); }
                         var timeToShow = (timeRemaining) ? timeRemaining : '';
                         $('#remaining').html(timeToShow);
                     } else if (data["team-sport-content"]["league-content"]["competition"]["result-scope"]["competition-status"] == 'complete') {
