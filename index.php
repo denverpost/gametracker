b48339070d1e3ed0e96af7cc03fc5aea0fa8718d
schneidan <dan@scrollwright.com>
2015-09-16 15:10:56 -0600

    Add GA tags to main index page

--- index.php
+++ index.php
@@ -30,6 +30,7 @@
 <meta name="language" content="en, sv" />
 <meta name="Copyright" content="Copyright &copy; The Denver Post." />
 <link rel="canonical" href="http://gametracker.denverpost.com/" />
+<meta name="description" content="Denver Post Gametrackers show live scores, social media posts and other updates from local pro and college sports games."
 <meta name="news_keywords" content="Denver, Broncos, Colorado, Rockies, Avalanche, Nuggets, CU, CSU, Rams, Buffs, Buffaloes, NBA, NHL, NFL, playoffs, stats, live, video, photos, score, updates" />
 
 <link rel="icon" href="http://extras.mnginteractive.com/live/media/favIcon/dpo/favicon.ico" type="image/x-icon" />
@@ -41,37 +42,6 @@
 <script type="text/javascript" src="custom.modernizr.js"></script>
 <script type="text/javascript" src="//local.digitalfirstmedia.com/common/dfm/assets/js/dfm-core-level1.js"></script>
 
-
-<script type="text/javascript">
-//Configure DFM Variables
-    dfm.api("data","siteId",          "Gametracker"); //Assign a siteid for your property
-    dfm.api("data","siteName",        "Denver Post Gametracker"); //Assign user-friendly name for site
-    dfm.api("data","siteDomain",      "gametracker.denverpost.com"); //Domain of the News.com, WordPress, Blog
-    dfm.api("data","actualDomain",    "gametracker.denverpost.com");//Full URl of site
-    dfm.api("data","localCommonUrl",  "");
-    dfm.api("data","localAssetsUrl",  "");
-    dfm.api("data","contentId",       "Denver Post Gametracker");
-    dfm.api("data","sectionName",     "Denver Post Gametracker"); //Omniture section/category name
-    dfm.api("data","pageId",          "");
-    dfm.api("data","pageUrl",         "http://gamecenter.denverpost.com/"); //Full URl of site
-    dfm.api("data","pageVanityUrl",   "http://gamecenter.denverpost.com/"); //Full URl of site
-    dfm.api("data","pageTitle",       "Gametracker from The Denver Post");
-    dfm.api("data","pageType",        "");
-    dfm.api("data","abstract",        "Denver Post Gametracker: Live updates, news, photos, videos and more");
-    dfm.api("data","keywords",        "Denver, Broncos, Colorado, Rockies, Avalanche, Nuggets, CU, CSU, Rams, Buffs, Buffaloes, NBA, NHL, NFL, playoffs, stats, live, video, photos, score, updates");
-    dfm.api("data","title",           "Denver Post Gametracker: Live updates, news, photos, videos and more");
-    dfm.api("data","sectionId",       "Denver Post Gametracker");
-    dfm.api("data","slug",            "gametracker");
-    dfm.api("data","byline",          "The Denver Post");
-    dfm.api("data","NetworkID",       "8013");
-    dfm.api("data","Taxonomy1",       "Gametracker");
-    dfm.api("data","Taxonomy2",       "");
-    dfm.api("data","Taxonomy3",       "");
-    dfm.api("data","Taxonomy4",       "");
-    dfm.api("data","kv",              "News");
-    dfm.api("data","omnitureAccount", "denverpost"); //Omniture s_account.
-</script>
-
 <script type="text/javascript">
     //configure Chartbeat variables
     var _sf_startpt=(new Date()).getTime();
@@ -81,158 +51,68 @@
 
 <body class="gametracker">
 
-<div id="omniture">
-    <script language="JavaScript"><!--//
-        /* Specify the Report Suite ID(s) to track here */
-        var s_account= dfm.api("data","omnitureAccount");
-        //-->
+<!-- Google Tag Manager Data Layer -->
+    <script>
+    var is_mobile = function() {
+      var check = false;
+      (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+      if ( check == true ) return 'YES';
+      return 'NO';
+    };
+    analyticsEvent = function() {};
+    analyticsSocial = function() {};
+    analyticsVPV = function() {};
+    analyticsClearVPV = function() {};
+    analyticsForm = function() {};
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push({
+        'ga_ua':'UA-61435456-7',
+        'quantcast':'p-4ctCQwtnNBNs2',
+        'quantcast label': 'Denver',
+        'comscore':'6035443',
+        'errorType':'',
+        'Publisher Domain':'denverpost.com',
+        'Publisher Product':'denverpost.com',
+        'Dateline':'',
+        'Publish Hour of Day':'',
+        'Create Hour of Day':'',
+        'Update Hour of Day':'',
+        'Behind Paywall':'NO',
+        'Mobile Presentation':is_mobile(),
+        'kv':'Sports',
+        'Release Version':'',
+        'Digital Publisher':'',
+        'Platform':'custom',
+        'Section':'Gametracker',
+        'Taxonomy1':'Denver Post Gametracker',
+        'Taxonomy2':'Gametracker',
+        'Taxonomy3':'',
+        'Taxonomy4':'',
+        'Taxonomy5':'',
+        'Content Source':'',
+        'Canonical URL': '',
+        'Slug':'',
+        'Content ID':'',
+        'Page Type':'special',
+        'Publisher State':'CO',
+        'Byline':'',
+        'Content Title':'',
+        'URL':'',
+        'Page Title':'',
+        'User ID':''
+    });
     </script>
-    <script type="text/javascript" language="JavaScript" src='http://extras.mnginteractive.com/live/js/omniture/SiteCatalystCode_H_22_1_NC.js'></script>
-    <script type="text/javascript" language="JavaScript" src='http://extras.mnginteractive.com/live/js/omniture/OmniUserObjAndHelper.js'></script>
-    <script language="JavaScript"><!--//
-        //Local Variables
-        var contentId = dfm.api("data","contentId");
-        var PaperBrand = getBrand2(s_account);
-        var PageName = dfm.api("data","pageTitle");
-        var SectionName = dfm.api("data","sectionName");
-
-        if (contentId != "" && contentId != null && SectionName == "") {
-            SectionName = dfm.api("data","sectionId");
-        }
-        var FriendlyName = SectionName;
-        var ArticleTitle = "";
-        if (contentId != "" && contentId != null) {
-            ArticleTitle = dfm.api("data","pageTitle");
-            FriendlyName = FriendlyName + " / "  +  ArticleTitle;
-        }
-        var domainName = getDomainName();
-        userObj = new omniObj();
-        userObj.load();
-        userObj.update();
-        userObj.save();
-        /* You may give each page an identifying name, server, and channel on the next lines. */
-        s.pageName=FriendlyName;
-        s.channel=SectionName; // Same as prop1
-        s.server="";// Blank
-        s.pageType=""; // Error pages ONLY
-        s.prop1="D=g";
-        beanprop2 = ("" != "") && ("" != null) ? "" : "?";
-        var escbeanprop2 = escape(beanprop2);
-        var unescbeanprop2 = unescape(escbeanprop2);
-        var articleId = dfm.api("data","contentId");        
-        beanprop3 = ("" != "") && ("" != null) ? "" : "?";
-        beanprop4 = ("" != "") && ("" != null) ? "" : "?";
-        if(articleId != "" && articleId != null){
-            beanprop5 = ("" != "") && ("" != null) ? "" : articleId +"_" +ArticleTitle;
-        } else {
-            beanprop5 = "?" ;
-        }
-        s.prop2='D=ch+' + "\"/" + unescbeanprop2 + "\"";
-        s.prop3='D=ch+' + "\"/" + unescbeanprop2 + "/" + beanprop3 + "\"";
-        s.prop4='D=ch+' + "\"/" + unescbeanprop2 + "/"+ beanprop3 + "/"+ beanprop4 + "\""; // Sub section 3  
-        s.prop5='D=ch+' + "\"/" + unescbeanprop2 + "/" + beanprop3 + "/" + beanprop4 + "/" + beanprop5 + "\""; // Sub section 4
-        s.prop6=dfm.api("data","Taxonomy1"); // Global - Section  TODO
-        s.prop7=dfm.api("data","Taxonomy2"); // Global - Sub section 1  TODO
-        s.prop8=dfm.api("data","Taxonomy3"); // Global - Sub section 2  TODO
-        var sourceVal = ""; // Source of request, i.e. RSS, flash, etc...
-        if(articleId != '' && articleId != null){
-            if(s.prop9 == '' || s.prop9 == null){
-                s.prop9 = sourceVal;
-            }
-        }
-        s.prop10=""; // Reserved for RSS
-        if(articleId != '' && articleId != null){
-            s.prop11=ArticleTitle=="null"?"":'D="'+domainName+' / "+v26+" / "+c50';
-        }
-        s.prop12=dfm.api("data","byline");
-        s.prop13=""; // Reserved for article
-        s.prop14=""; // Reserved for article
-        s.prop15=""; // Reserved for article
-        s.prop16=""; // Search
-        s.prop17=""; // Search
-        s.prop18=""; // Search
-        s.prop19=""; // Search
-        s.prop20=""; // Reserved for Search
-        s.prop21=""; // Reserved for Search
-        s.prop22=""; // Reserved for Search
-        s.prop23=""; // Reserved for Search
-        s.prop24=""; // Reserved for Search
-        s.prop25=""; // Reserved for Search
-        s.prop26=""; // 3rd Party Vendors
-        s.prop27=""; // OneSpot
-        s.prop28=""; // Blank
-        if(s.prop29 == null || s.prop29 == ''){
-            s.prop29="";
-        }
-        s.prop30=""; // Form Analysis Plugin
-        s.prop31=""; // Blank
-        s.prop32=""; // Blank
-        s.prop33=ArticleTitle=="null"?'D=c40+" / "+c43':'D=c40+" / "+c50+" / "+c9';
-        s.prop34=ArticleTitle=="null"?'D=c40+" / "+c43':'D=c40+" / "+c43+" / "+c50';
-        s.prop35=ArticleTitle=="null"?'D=v18=" / "+c40+" / "+c43':'D=v18+" / "+c40+" / "+c50';
-        s.prop36=isCampaign(getCampaignValue("EADID")+getCampaignValue("CREF"), PaperBrand, PageName, ArticleTitle); // Campaign Tracking Code  + Paper Brand + Page Name
-        s.prop37=isCampaign(getCampaignValue("IADID")+getCampaignValue("SOURCE"), PaperBrand, PageName, ArticleTitle); // Affiliate ID + Paper Brand + Page Name
-        s.prop38=isCampaign(getCampaignValue("PARTNERID"), PaperBrand, PageName,ArticleTitle); // Internal Referral ID + Paper Brand + Page Name
-        s.prop39="";                                                             // Search Engine + Keywords + Paper Brand + Page Name (populated by functions.js)
-        s.prop40=PaperBrand;                                                     // Paper Brand
-        s.prop41="";                                                             // Blank
-        s.prop42="";
-        s.prop43=SectionName; //
-        s.prop44=ArticleTitle=="null"?"":'D=c40+" / "+c43+" / "+v26';
-        s.prop45=ArticleTitle=="null"?"":'D=c40+" / "+c43+" / "+c12';
-        s.prop46=getArticleHelperPage(domainName,"22133898",location.href,ArticleTitle); // ArticleID + Special Page Name + Article Title
-        s.prop47=""; // Search
-        s.prop48=""; // Blank
-        s.prop49=""; // Blank
-        s.prop50=ArticleTitle;
-        /* E-commerce Variables */
-        s.campaign=getCiQueryString("EADID")+getCiQueryString("CREF");          //External Campaign - ?EADID=id
-        s.state="";
-        s.zip="";
-        s.events=getEvents(ArticleTitle, s.events);
-        s.products="";
-        s.purchaseID="";
-        s.eVar1=getCiQueryString("PARTNERID");                                  // Internal Campaign - ?PID=id
-        s.eVar2=getCiQueryString("IADID")+getCiQueryString("SOURCE");           // Affiliate ID - ?IADID=id
-        s.eVar3=getBrandOnChange(PaperBrand);                                             //Paper Brand
-        s.eVar4="D=pageName";
-        s.eVar5="";
-        s.eVar6="";
-        s.eVar7="";
-        s.eVar8="";
-        s.eVar9="";
-        s.eVar10="";
-        s.eVar11="";
-        s.eVar12="";
-        s.eVar13="";
-        s.eVar14=userObj.fPage|userObj.conPage|userObj.loginConPage?userObj.vType:''; //Visitory Type
-        s.eVar15=userObj.fPage|userObj.userIdChange?userObj.userId:'';                     // User ID
-        s.eVar16=userObj.conPage?userObj.rType:'';
-        s.eVar17="";
-        s.eVar18=getUserType();
-        s.eVar19=userObj.fPage|userObj.conPage|userObj.aaPage?userObj.regStatus:''; //Registration Status
-        s.eVar20=""; // Search
-        s.eVar21="";
-        s.eVar22="";
-        s.eVar23="";                                                      // Refinements
-        s.eVar24="D=c43";
-        s.eVar25="";
-        s.eVar26=articleId;  // article ID
-        s.eVar50=getCiQueryString("AADID");
-        //-->
-    </script>
-    <script type="text/javascript" src='http://extras.mnginteractive.com/live/js/omniture/functions.js'></script>
-    <script type="text/javascript"><!--//
-        var s_code=s.t();if(s_code)document.write(s_code);
-        //-->
-    </script>
-    <script language="JavaScript"><!--//
-        if(navigator.appVersion.indexOf('MSIE')>=0)document.write(unescape('%3C')+'\!-'+'-');
-        //-->
-    </script>
-    <noscript><img src="http://NeBnr.112.2O7.net/b/ss/NeBnr/1/H.17--NS/0" height="0" width="0" border="0" alt="" style="margin:0;padding:0;" /></noscript>
-    <!-- End SiteCatalyst code version: H.17 -->
-</div>
+    <!-- End Google Tag Manager Data Layer -->
+    <!-- Google Tag Manager --><noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-TLFP4R" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><script>
+    (function(w,d,s,l,i) {
+    w[l]=w[l]||[];
+    w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+    var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+    j.async=true;
+    j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })
+    (window,document,'script','dataLayer','GTM-TLFP4R');
+    </script><!-- End Google Tag Manager -->
 
 <div id="topper">
     
