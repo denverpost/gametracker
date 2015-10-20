f8c51405a221ea2a598a6df92c7bd900675ca9e9
schneidan <dan@scrollwright.com>
2015-10-20 12:55:13 -0600

    Set up JS to remove saving URL parameter after a save so you con't hit Conform Resubmission errors.

--- editor/index.php
+++ editor/index.php
@@ -675,13 +675,30 @@ function editAds() {
 
 <script type="text/javascript" src="../js/foundation.min.js"></script>
 <script>
+	function removeParam(key, sourceURL) {
+	    var rtn = sourceURL.split("?")[0],
+	        param,
+	        params_arr = [],
+	        queryString = (sourceURL.indexOf("?") !== -1) ? sourceURL.split("?")[1] : "";
+	    if (queryString !== "") {
+	        params_arr = queryString.split("&");
+	        for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+	            param = params_arr[i].split("=")[0];
+	            if (param === key) {
+	                params_arr.splice(i, 1);
+	            }
+	        }
+	        rtn = rtn + "?" + params_arr.join("&");
+	    }
+	    return rtn;
+	}
 	$(document).foundation();
 	$(document).ready(function(){
 		if (location.hash == '#changelog') {
 			$('#changeLog').foundation('reveal', 'open');
 		}
 	});
-	
+	history.replaceState(history.state,document.title,removeParam('saving',window.location.href));
 </script>
 
 </body>
